bd7fae5d7dba6e3b9cf5e5a4774f9bd66f32f01c
Sahil <n.sahil2709@gmail.com>
2026-03-17 16:23:31 +0530

    learned how to connect postgresql to my project and migrated fields to the database

--- django-learning-main/myproject/myproject/settings.py
+++ django-learning-main/myproject/myproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,8 +75,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Project',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
--- django-learning-main/myproject/travello/views.py
+++ django-learning-main/myproject/travello/views.py
@@ -8,18 +8,21 @@ def index(request):
   dest1.desc = 'Nulla pretium tincidunt felis, nec.'
   dest1.img = 'Destination_1.jpg'
   dest1.price = 701
+  dest1.offer = False
 
   dest2 = Destination()
   dest2.name = 'Hyderabad'
   dest2.desc = 'Nulla pretium tincidunt felis, nec.'
   dest2.img = 'Destination_2.jpg'
   dest2.price = 891
+  dest2.offer = True
 
   dest3 = Destination()
   dest3.name = 'Chennai'
   dest3.desc = 'Nulla pretium tincidunt felis, nec.'
   dest3.img = 'Destination_3.jpg'
   dest3.price = 801
+  dest3.offer = False
 
   dest = [dest1, dest2, dest3]
   return render(request, "index.html", {'dest': dest})

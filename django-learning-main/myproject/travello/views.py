0e8a993c683af654f4d9cd76cb0467d6bf6cc4db
Sahil <n.sahil2709@gmail.com>
2026-03-17 15:30:23 +0530

    Project updation into simpler and easier format

--- django-learning-main/myproject/travello/views.py
+++ django-learning-main/myproject/travello/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 def index(request):
-  return render(request, "index.html")
+
+  dest1 = Destination()
+  dest1.name = 'Mumbai'
+  dest1.desc = 'Nulla pretium tincidunt felis, nec.'
+  dest1.img = 'Destination_1.jpg'
+  dest1.price = 701
+
+  dest2 = Destination()
+  dest2.name = 'Hyderabad'
+  dest2.desc = 'Nulla pretium tincidunt felis, nec.'
+  dest2.img = 'Destination_2.jpg'
+  dest2.price = 891
+
+  dest3 = Destination()
+  dest3.name = 'Chennai'
+  dest3.desc = 'Nulla pretium tincidunt felis, nec.'
+  dest3.img = 'Destination_3.jpg'
+  dest3.price = 801
+
+  dest = [dest1, dest2, dest3]
+  return render(request, "index.html", {'dest': dest})

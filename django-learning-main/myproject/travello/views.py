9f80c371d825edb59293e40a301b19753bf73051
Sahil <n.sahil2709@gmail.com>
2026-03-17 16:55:12 +0530

    Added Destination model with PostgreSQL integration and registered it in Django admin for dynamic content management

--- django-learning-main/myproject/travello/views.py
+++ django-learning-main/myproject/travello/views.py
@@ -3,26 +3,5 @@ from .models import Destination
 # Create your views here.
 def index(request):
 
-  dest1 = Destination()
-  dest1.name = 'Mumbai'
-  dest1.desc = 'Nulla pretium tincidunt felis, nec.'
-  dest1.img = 'Destination_1.jpg'
-  dest1.price = 701
-  dest1.offer = False
-
-  dest2 = Destination()
-  dest2.name = 'Hyderabad'
-  dest2.desc = 'Nulla pretium tincidunt felis, nec.'
-  dest2.img = 'Destination_2.jpg'
-  dest2.price = 891
-  dest2.offer = True
-
-  dest3 = Destination()
-  dest3.name = 'Chennai'
-  dest3.desc = 'Nulla pretium tincidunt felis, nec.'
-  dest3.img = 'Destination_3.jpg'
-  dest3.price = 801
-  dest3.offer = False
-
-  dest = [dest1, dest2, dest3]
+  dest = Destination.objects.all()
   return render(request, "index.html", {'dest': dest})
